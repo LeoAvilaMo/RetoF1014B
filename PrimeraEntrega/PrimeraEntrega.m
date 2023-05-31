7289205588fe4105c17eef8bde0aeb516ce191a7
J-C-1020 <80874022+J-C-1020@users.noreply.github.com>
2023-05-30 18:19:29 -0600

    Update PrimeraEntrega.m

--- PrimeraEntrega/PrimeraEntrega.m
+++ PrimeraEntrega/PrimeraEntrega.m
@@ -13,9 +13,9 @@ spacez = -5:1:5;
 [X,Y,Z] = meshgrid(spacex,spacey,spacez);
 d_theta = (2*pi)/N;
 theta = 0:d_theta:((2*pi-d_theta));
-campo_Bx_total = zeros(size(X));
-campo_By_total = zeros(size(Y));
-campo_Bz_total = zeros(size(Z));
+Bx_total = zeros(size(X));
+By_total = zeros(size(Y));
+Bz_total = zeros(size(Z));
 R = sqrt(((X).^2+(Y).^2+(Z).^2));
 for i = 1:length(theta) %ciclo for que vaya por cada elemento de corriente en el aro
     ds = radio*theta(i); %diferencial de superficie del aro en base al angulo establecido por el vector de theta
@@ -25,12 +25,12 @@ for i = 1:length(theta) %ciclo for que vaya por cada elemento de corriente en el
     ri_y = Y - radio_dy; %vector del punto en el espacio menos el vector del radio en y
     ri_z = Z; %vector del punto en el espacio menos el vector del radio en z
     ri = sqrt((ri_x.^2)+(ri_y.^2)+(ri_z.^2+(0.2)^2)); %magnitud de los vectores anteriores
-    campo_Bx= (miu*I/4*pi) *((radio_dy.*ri_z)./ri.^3); %se obtiene cada campo en x,y y z
-    campo_By= -(miu*I/4*pi) *((radio_dx.*ri_z)./ri.^3);
-    campo_Bz= (miu*I/4*pi) *((radio_dx.*ri_y - radio_dy.*ri_x)./ri.^3);
-    campo_Bx_total = campo_Bx_total + campo_Bx; %se actualia las variables para ser sumadas en cada ciclo
-    campo_By_total = campo_By_total + campo_By;
-    campo_Bz_total = campo_Bz_total + campo_Bz;
+    Bx= (miu*I/4*pi) *((radio_dy.*ri_z)./ri.^3); %se obtiene cada campo en x,y y z
+    By= -(miu*I/4*pi) *((radio_dx.*ri_z)./ri.^3);
+    Bz= (miu*I/4*pi) *((radio_dx.*ri_y - radio_dy.*ri_x)./ri.^3);
+    Bx_total = Bx_total + Bx; %se actualia las variables para ser sumadas en cada ciclo
+    By_total = By_total + By;
+    Bz_total = Bz_total + Bz;
 end
 
 
@@ -42,11 +42,15 @@ z = zeros(size(theta));
 figure;
 plot3(x, y, z, 'b', 'LineWidth', 2);
 hold on 
-%se gráfican los campo
-quiver3(X,Y,Z, campo_Bx_total, campo_By_total, campo_Bz_total)
+%se gráfican los campos
+quiver3(X,Y,Z, Bx_total, By_total, Bz_total)
 
-y2 = round((length(campo_By_total))/2);
-Bx1 = squeeze(campo_Bx_total(:,y2,:));
-Bz1 = squeeze(campo_Bz_total(:,y2,:));
-figure;
-streamslice(spacex,spacez,Bx1',Bz1',2);
+hold off
+%figure;
+i= [-radio radio];
+j = [0 0];
+origen = round((length(spacey))/2);
+Bx1 = squeeze(Bx_total(:,y2,:));
+Bz1 = squeeze(Bz_total(:,y2,:));
+plot(i,j);
+streamslice(spacex,spacez,Bx1',Bz1');
